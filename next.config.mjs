38e8cc99edcf4a3d9ba8e063d107c90d8eb3bce1
khamudom <khamu@outlook.com>
2025-07-19 09:11:54 -0700

    add remotePatterns configuration needed to allow images from Supabase storage in next config

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,14 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
   devIndicators: false,
   // Allow cross-origin requests from local network devices for development
